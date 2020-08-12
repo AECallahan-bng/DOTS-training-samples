9d849855880e119d5c9deaaec27b55ce60e0c02f
lauram-bng <lmanresa@bungie.com>
2020-08-12 17:21:24 -0400

    Crop Sell System
    Crop Sell System created

---
+++ Ported/AutoFarmers/Assets/Scripts/Systems/Ai/AiCommandSell.cs
@@ -0,0 +1,62 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class AiCommandSell : SystemBase
+{
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+    protected override void OnUpdate()
+    {
+        var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
+
+        var query = GetEntityQuery(typeof(SectionWorldGrid));
+        var buffer = GetBuffer<SectionWorldGrid>(query.GetSingletonEntity());
+        var size = GetSingleton<GridSize>();
+        int2 sizeInt = new int2(size.Width, size.Height);
+
+        var droneResources = GetSingleton<DroneResources>();
+        var farmerResources = GetSingleton<FarmerResources>();
+        var farmResourcesEntity = GetSingletonEntity<DroneResources>();
+
+        Entities
+            .WithAll<AiTagCommandSell>()
+            .ForEach((
+            int entityInQueryIndex, 
+            ref Entity carrierEntity, 
+            ref AiCarriedObject carriedObjectComponent, 
+            in Translation translationComponent) => 
+        {
+            int2 pos = new int2((int)translationComponent.Value.x, (int)translationComponent.Value.z);
+
+            Entity entityInPos = buffer[PosToIndex(sizeInt, pos)].Value;
+
+            if(HasComponent<CellTagTeleporter>(entityInPos))
+            {
+                ecb.AddComponent<CropSellingTag>(entityInQueryIndex, carriedObjectComponent.CarriedObjectEntity);
+                ecb.RemoveComponent<AiCarriedObject>(entityInQueryIndex, carrierEntity);
+
+                droneResources.Resources++;
+                farmerResources.Resources++;
+
+                ecb.SetComponent(entityInQueryIndex, farmResourcesEntity, droneResources);
+                ecb.SetComponent(entityInQueryIndex, farmResourcesEntity, farmerResources);
+
+                ecb.RemoveComponent<AiTagCommandSell>(entityInQueryIndex, carrierEntity);
+                ecb.AddComponent<AiTagCommandIdle>(entityInQueryIndex, carrierEntity);                
+            }
+        }).ScheduleParallel();
+
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
+    }
+
+    static int PosToIndex(int2 size, int2 pos)
+    {
+        int i = pos.y * size.x + pos.x;
+        return i;
+    }
+}
