8c4495fff35fb21c650eeacf6da76e494dcaa578
AECallahan-bng <aecallahan@bungie.com>
2020-08-12 15:54:59 -0700

    Update AiMoveSystem.cs

--- Ported/AutoFarmers/Assets/Scripts/Systems/Ai/AiMoveSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/Ai/AiMoveSystem.cs
@@ -15,13 +15,13 @@ public class AiMoveSystem : SystemBase
         Entities.ForEach((ref Translation currentPosition, in AiTargetCell moveTarget) => {
 			float2 direction = new float2(moveTarget.CellCoords.x, moveTarget.CellCoords.y) - new float2(currentPosition.Value.x, currentPosition.Value.z);
 
-			if (direction.x > direction.y)
+			if (math.abs(direction.x) > math.abs(direction.y))
 			{
-				currentPosition.Value.x += deltaTime * cMovementSpeed;
+				currentPosition.Value.x += deltaTime * cMovementSpeed * math.sign(direction.x);
 			}
 			else
 			{
-				currentPosition.Value.z += deltaTime * cMovementSpeed;
+				currentPosition.Value.z += deltaTime * cMovementSpeed * math.sign(direction.y);
 			}
 
 		}).ScheduleParallel();
