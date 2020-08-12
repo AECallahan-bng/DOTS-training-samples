265c0814ec1e3d26efca5d24da3a2496acaf865b
lauram-bng <lmanresa@bungie.com>
2020-08-12 18:34:24 -0400

    Ai Command Plant System
    Added Planting System, updated crop prefab, updated Selling System

--- Ported/AutoFarmers/Assets/Scripts/Systems/Ai/AiCommandSell.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/Ai/AiCommandSell.cs
@@ -14,46 +14,30 @@ public class AiCommandSell : SystemBase
     {
         var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
 
-		if (HasSingleton<SectionWorldTag>())
-		{
-			var query = GetEntityQuery(typeof(SectionWorldTag));
-			var buffer = GetBuffer<SectionWorldGrid>(query.GetSingletonEntity());
-			var size = GetSingleton<GridSize>();
-			int2 sizeInt = new int2(size.Width, size.Height);
-
-			Entities
-				.WithAll<AiTagCommandSell>()
-				.WithNativeDisableParallelForRestriction(buffer)
-				.ForEach((
+		Entities
+			.WithAll<AiTagCommandSell>()
+			.ForEach((
 				int entityInQueryIndex,
 				ref Entity carrierEntity,
 				ref AiCarriedObject carriedObjectComponent,
+				in AiTargetCell targetCell,
 				in Translation translationComponent) =>
 			{
 				int2 pos = new int2((int)translationComponent.Value.x, (int)translationComponent.Value.z);
 
-				Entity entityInPos = buffer[PosToIndex(sizeInt, pos)].Value;
-
-				if (HasComponent<CellTagTeleporter>(entityInPos))
+				if (pos.Equals(targetCell.CellCoords))
 				{
 					ecb.AddComponent<CropSellingTag>(entityInQueryIndex, carriedObjectComponent.CarriedObjectEntity);
 					ecb.RemoveComponent<AiCarriedObject>(entityInQueryIndex, carrierEntity);
 
 					Entity transaction = ecb.CreateEntity(entityInQueryIndex);
-					ecb.AddComponent<SellTransaction>(entityInQueryIndex, transaction, new SellTransaction() { Resources = 1, GridPosition = pos });
+					ecb.AddComponent(entityInQueryIndex, transaction, new SellTransaction() { Resources = 1, GridPosition = pos });
 
 					ecb.RemoveComponent<AiTagCommandSell>(entityInQueryIndex, carrierEntity);
 					ecb.AddComponent<AiTagCommandIdle>(entityInQueryIndex, carrierEntity);
 				}
 			}).ScheduleParallel();
-		}
 
-        m_ECBSystem.AddJobHandleForProducer(Dependency);
-    }
-
-    static int PosToIndex(int2 size, int2 pos)
-    {
-        int i = pos.y * size.x + pos.x;
-        return i;
+		m_ECBSystem.AddJobHandleForProducer(Dependency);
     }
 }
