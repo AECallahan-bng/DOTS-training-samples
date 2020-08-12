74864084cc961814cfb7d3f9d77f866595d3726e
cbutcher-bng <cbutcher@bungie.com>
2020-08-12 13:06:39 -0700

    farmers spawn at last resource location

--- Ported/AutoFarmers/Assets/Scripts/Systems/SpawnFarmersSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/SpawnFarmersSystem.cs
@@ -2,21 +2,34 @@
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using UnityEngine;
 
 [UpdateInGroup(typeof(InitializationSystemGroup))]
 public class SpawnFarmersSystem : SystemBase
 {
+	private Random m_Random;
+
+	protected override void OnCreate()
+	{
+		m_Random = new Random(0x1234567);
+	}
+
 	protected override void OnUpdate()
 	{
 		var ecb = new EntityCommandBuffer(Allocator.TempJob);
+		var random = m_Random;
+
 		Entities.ForEach((ref FarmerResources resources, in FarmerCost farmerCost, in FarmContent farmContent) =>
 		{
 			int newResources = resources.Resources;
 
 			while (newResources >= farmerCost.Value)
 			{
-				ecb.Instantiate(farmContent.Farmer);
+				float3 spawnPosition = new float3(resources.LastGridPosition.x + 0.5f, 0, resources.LastGridPosition.y + 0.5f);
+				// float3 spawnPosition = MapGenerationSystem::GetCellMidpoint(resources.LastGridPosition);
+
+				var farmerEntity = ecb.Instantiate(farmContent.Farmer);
+				ecb.SetComponent(farmerEntity, new Unity.Transforms.Translation() { Value = spawnPosition });
+
 				newResources -= farmerCost.Value;
 			}
 
@@ -24,6 +37,8 @@ public class SpawnFarmersSystem : SystemBase
 		}).Run();
 		ecb.Playback(EntityManager);
 		ecb.Dispose();
+
+		m_Random = random;
 	}
 }
 
