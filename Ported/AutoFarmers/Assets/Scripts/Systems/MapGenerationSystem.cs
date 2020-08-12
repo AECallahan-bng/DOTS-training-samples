56909d87ec778d67d42e3453c5d2a98bd0f38c02
Stephanie <stephanier@unity3d.com>
2020-08-12 13:32:15 -0400

    Map Generation system

---
+++ Ported/AutoFarmers/Assets/Scripts/Systems/MapGenerationSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class MapGenerationSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+    }
+
+    protected override void OnUpdate()
+    {
+
+
+    }
+}
+
