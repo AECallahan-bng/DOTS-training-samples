d6474e8937e9281a91e4459ed75d4423924310fc
Stephanie <stephanier@unity3d.com>
2020-08-13 17:16:09 -0400

    break it!

--- Ported/AutoFarmers/Assets/Scripts/Components/GridData/SectionWorldGrid.cs
+++ Ported/AutoFarmers/Assets/Scripts/Components/GridData/SectionWorldGrid.cs
@@ -5,3 +5,13 @@ public struct SectionWorldGrid : IBufferElementData
 {
     public Entity Value;
 }
+public struct Ground : IComponentData
+{
+    public Entity Value;
+
+}
+public struct Over : IComponentData
+{
+    public Entity Value;
+
+}
